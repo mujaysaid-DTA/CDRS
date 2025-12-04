8baad1f82c3c29c168a3b28ea24d6105d54837cd
Abdullahi Said Abdulkadir <mujaysaid@gmail.com>
2025-12-04 01:40:38 +0100

    Fixed blank screen and restored main app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-// --- THE FIX IS HERE ---
-// We changed "./IncidentList" to "./pages/IncidentList"
-import IncidentList from './pages/IncidentList';
+import App from './App';  // <--- We are switching back to the Main App
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <IncidentList />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
